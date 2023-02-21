0f533012f3d2765ec6240639d44037bf3c32c40d
Sveta Pelipas <rannylay@bk.ru>
2023-02-21 19:13:28 +0300

    Добавьте файлы проекта.

---
+++ поздравлятор/Birthday.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+
+namespace поздравлятор
+{
+    class Birthday
+    {
+        public string Name;
+        public int Day;
+        public int Month;
+        public int Year;
+        public bool IsTexted = false;
+        public Birthday(string name, int day, int month, int year)
+        {
+            Name = name;
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+    }
+}
